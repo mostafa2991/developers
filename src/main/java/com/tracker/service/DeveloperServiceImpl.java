29f889f0f9a0c6f26f30909ab50a3f4bad443c4e
mostafa <mostafa.mm661@gmail.com>
2022-04-15 21:40:49 +0200

    Removed folder from repository

--- src/main/java/com/tracker/service/DeveloperServiceImpl.java
+++ src/main/java/com/tracker/service/DeveloperServiceImpl.java
@@ -39,9 +39,9 @@ public class DeveloperServiceImpl implements DeveloperService {
     }
 
     @Override
-    public DeveloperDto updateDeveloper(DeveloperDto developerDto) {
-        Developer developer = Optional.ofNullable(developerRepository.findByName(developerDto.getName()))
-                .orElseThrow(() -> new NotFoundException("can't find developer with name: " + developerDto.getName()));
+    public DeveloperDto updateDeveloper(String name, DeveloperDto developerDto) {
+        Developer developer = Optional.ofNullable(developerRepository.findByName(name))
+                .orElseThrow(() -> new NotFoundException("can't find developer with name: " + name));
         developer = DeveloperMapper.INSTANCE.dtoToEntity(developerDto);
         Developer updateDeveloper = developerRepository.save(developer);
         return DeveloperMapper.INSTANCE.entityToDto(updateDeveloper);
@@ -65,44 +65,6 @@ public class DeveloperServiceImpl implements DeveloperService {
         return DeveloperMapper.INSTANCE.entityToDto(savedDeveloper);
     }
 
-
-//    @Override
-//    public DeveloperDto getDroneBySerialNumber(String serialNumber) {
-//
-//        Developer developer = getDroneEntityBySerialNumber(serialNumber);
-//        DeveloperDto droneDto = DeveloperMapper.INSTANCE.entityToDto(developer);
-//        return droneDto;
-//
-//    }
-//    private Developer getDroneEntityBySerialNumber(String serialNumber) {
-//        Developer developer = Optional.ofNullable(developerRepository.findBySerialNumber(serialNumber)).
-//                orElseThrow(() -> new NotFoundException("can't find drone with serial number: " + serialNumber));
-//        return developer;
-//    }
-//    @Override
-//    public List<DeveloperDto> getAvailableDroneForLoading() {
-//        if (countDrones() == 0) throw new GenericExceptionResponse("No drones found");
-//        List<DeveloperDto> dronesDtos =
-//                Optional.ofNullable(developerRepository.getAvailableDroneForLoading())
-//                        .map(entities -> entities.stream()
-//                                .map(DeveloperMapper.INSTANCE::entityToDto)
-//                                .collect(Collectors.toList()))
-//                        .orElseThrow(() -> new NotFoundException("No drones Available for loading."));
-//        return dronesDtos;
-//    }
-//    private long countDrones() {
-//        return developerRepository.count();
-//    }
-//    private void checkMaxDronesCountLimit(int maxCount) {
-//        long count = countDrones();
-//        if (count == maxCount) throw new FullDroneException();
-//    }
-//    private boolean checkDuplicates(DeveloperDto droneDto) {
-//        Developer developer = developerRepository.findBySerialNumber(droneDto.getSerialNumber());
-//        if (Objects.nonNull(developer))
-//            throw new GenericExceptionResponse("The serial number for drone is already exist:" + droneDto.getSerialNumber());
-//        return true;
-//    }
 
 
 }
